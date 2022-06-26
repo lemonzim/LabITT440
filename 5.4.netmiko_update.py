82bb1250452b2405acf20b792f0cf3a3fe763b66
Hazim <99800148+lemonzim@users.noreply.github.com>
2022-06-26 22:52:11 +0800

    Create 5.4.netmiko_update.py

---
+++ 5.4.netmiko_update.py
@@ -0,0 +1,14 @@
+from netmiko import ConnectHandler
+
+myserver = {
+    'device_type': 'linux',
+    'host':   '192.168.56.105',  #Server IP
+    'username': 'hazim_ubuntu', #Server Username
+    'password': '1Ii@()#@))!', #server password
+    'port' : 22,
+    'secret': '',
+}
+
+net_connect = ConnectHandler(**myserver)
+output = net_connect.send_command('sudo apt update')
+print(output)
